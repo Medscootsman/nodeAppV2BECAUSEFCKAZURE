1ba298662101eec0930bd073fa418a128578e3f0
Medscootsman <murraylyne2@Gmail.com>
2016-11-21 16:36:45 +0000

    connect.js added.

--- insert.js
+++ insert.js
@@ -2,7 +2,7 @@
  * Created by 1607084 on 21/11/2016.
  */
 //lets require/import the mongodb native drivers.
-var mongodb = require('mongodb');
+var mongodb = require("mongodb");
 //and our HTTP server
 var http = require('http');
 //setup our port
@@ -10,7 +10,7 @@ var port = process.env.PORT || 1337; //WHHHYYYYYYYYYYY SO MUCH
 
 var url = 'mongodb://testcats:cat@ds050879.mlab.com:50879/nodeapp';
 
-var MongoClient = mongodb.MongoClient;
+var MongoClient  = mongodb.MongoClient;
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write('Connecting \n');
@@ -19,39 +19,32 @@ http.createServer(function(request, response) {
         response.write('Connection Made \n');
         if (err) {
             response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
-            //Error so close connecgtion
+            //Error so close connection
             db.close();
         } else {
             //HURRAY!! We are connected. :)
             response.write('Connection established to' + url + "\n");
 
             // do some work here with the database.
-            //get the documents collection
+            // Get the documents collection
             var collection = db.collection('users');
-
             var user1 = {name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']};
-
             var user2 = {name: 'modulus user', age: 22, roles: ['user']};
-
-            var user3 = {
-                name: 'modulus super admin', age: 110, roles: ['super-admin', 'admin',
-                    'moderator', 'user']
-            };
-
+            var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};
             collection.insert([user1, user2, user3], function (err, result) {
                 if (err) {
                     response.write('Insert failed ' + err + "\n");
-
                 } else {
                     console.log(result);
-                    response.write("Inserted " + result.insertedCount + ' documents ok. + "\n"');
+                    response.write('Inserted ' + result.insertedCount + ' documents ok. +"\n"');
                 }
                 //Close connection
                 db.close();
                 response.end('Finished, Connection closed \n');
-
+                //remove any other db.close or response.end statement below this line
             });
         }
-    }).listen(port);}
-    
+    });
+
+}).listen(port);
 
